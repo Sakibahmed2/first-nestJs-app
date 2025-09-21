8415b32de4d892c8414bf4ecfde38a07286f7847
shakib-chahealth <mauijaloskor@gmail.com>
2025-09-21 11:36:11 +0600

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, UserController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get()
+  getUser(): string {
+    return 'This is user endpoint';
+  }
+}
