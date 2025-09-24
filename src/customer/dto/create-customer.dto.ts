a8aaf0d17614587da7a7656ea380f57738a9aee9
shakib-chahealth <mauijaloskor@gmail.com>
2025-09-24 19:19:56 +0600

    Validate DTOs Using class-validator & class-transformer

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { IsInt, IsString } from 'class-validator';
+
 export class CreateCustomerDto {
+  @IsString()
   name: string;
+
+  @IsInt()
   age: number;
 }
